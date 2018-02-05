42bac39911a82818eb7d0324397359d8fee0c2f0
Arseniy Batyrov <comolder@gmail.com>
2018-02-06 02:01:50 +0300

    Second commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,51 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Arsbatyrov on 2018-02-04.
+ */
+public class SampleTest {
+    RemoteWebDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("deviceName","Android Emulator");
+            capabilities.setCapability("platformName","Android");
+            capabilities.setCapability("platformVersion", "6.0");
+//            capabilities.setCapability("browserName", "Chrome");
+            capabilities.setCapability("app", "/Users/abatyrov/Documents/java_appium/appiumtutorial-master/ultimate_converter.apk");
+            capabilities.setCapability("appPackage:","com.physphil.android.unitconverterultimate");
+            capabilities.setCapability("appActivity:","com.physphil.android.unitconverterultimate.MainActivity");
+
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+//    @Test
+//    public void firstTest() {
+//        this.driver.get("http://google.com");
+//        Assert.assertEquals("Title should be Google", "Google", driver.getTitle());
+//    }
+
+    @Test
+    public void appTest() {
+        this.driver.findElementById("android:id/button1").click();
+//        Assert.assertEquals("Title should be Google", "Google", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
